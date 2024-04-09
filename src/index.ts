0fcf8231475db7baf7edbf7b2dc5f96b7ef6a090
Natchkung <natchnon654321@gmail.com>
2024-04-09 14:16:00 +0700

    test5555

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,35 @@
-// require('dotenv').config()
+require('dotenv').config()
 
 import express from 'express';
-// import http from 'http';
-// import bodyParser from 'body-parser';
-// import cookieParser from 'cookie-parser';
-// import compression from 'compression';
-// import cors from 'cors';
-// import mongoose from 'mongoose';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-// import router from './router';
+import router from './router';
 
 const app : express.Express = express();
 
-// app.use(cors({
-//     origin: true,
-//     credentials: true,
-// }));
+app.use(cors({
+    origin: true,
+    credentials: true,
+}));
 
 
-// app.use(compression());
-// app.use(cookieParser());
-// app.use(bodyParser.json());
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
-// mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGO_URL)
-// mongoose.connection.on('error', (error: Error) => console.log(error));
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URL)
+mongoose.connection.on('error', (error: Error) => console.log(error));
 
-// server.listen(process.env.SERVER_PORT || 3000, () => {
-//     console.log(`Sever running on port: ${process.env.SERVER_PORT || 3000}`);
-// });
+app.listen(process.env.SERVER_PORT || 3000, () => {
+    console.log(`Sever running on port: ${process.env.SERVER_PORT || 3000}`);
+});
 
-// app.use('/', router())
-
-app.get("/", (req, res) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+app.use('/', router())
