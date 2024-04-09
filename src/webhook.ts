fc0554bf5bd87f50f46726f75707fa5f2be57971
Natchkung <natchnon654321@gmail.com>
2024-04-09 16:47:41 +0700

    delete img webhook

--- src/webhook.ts
+++ src/webhook.ts
@@ -26,7 +26,6 @@ export const Discordwebhook = async (nameScript:string,ipaddress:string,username
                     'short': true
                 }
             ],
-            'image_url': 'https://i0.wp.com/www.printmag.com/wp-content/uploads/2021/02/4cbe8d_f1ed2800a49649848102c68fc5a66e53mv2.gif?fit=476%2C280&ssl=1',
             'footer_icon': 'https://media.discordapp.net/attachments/1133807349266653264/1176607928325787649/IMG-100000000.jpg?ex=661ed406&is=660c5f06&hm=6d689fabccd0a20c3498c8f6054b858972ad04cd3b20a12940ad728e5db1e542&=&format=webp&width=1118&height=671',
             'footer': 'UKB-DEVELOPER',
             'ts': Date.now() / 1_000
